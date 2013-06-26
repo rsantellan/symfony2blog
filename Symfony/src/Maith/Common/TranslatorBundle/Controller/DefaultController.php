83c04c30c35b1dcbe09790358c6ee0737e2dfe57
Rodrigo Santellan <rsantellan@gmail.com>
2013-06-25 23:05:42 -0300

    Se agregan mas parametros

--- Symfony/src/Maith/Common/TranslatorBundle/Controller/DefaultController.php
+++ Symfony/src/Maith/Common/TranslatorBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Maith\Common\TranslatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Translation\MessageCatalogue;
 
 class DefaultController extends Controller
 {
@@ -13,10 +14,22 @@ class DefaultController extends Controller
         $list = $this->container->getParameter('translation_bundles');
         if(!is_null($admin))
         {
-          var_dump($this->container->getParameter('admin_translation_bundles'));
+          $list = array_merge($list, $this->container->getParameter('admin_translation_bundles'));
         }
+        $lang_list = $this->container->getParameter('translation_languages');;
+        //$path = $this->get('kernel')->getBundle('LoopitaMetalizadoraBundle')->getPath();
+        return $this->render('MaithCommonTranslatorBundle:Default:index.html.twig', array('bundles' => $list, 'langs' => $lang_list));
+    }
+    
+    public function getTranslationAction(){
         
-        $path = $this->get('kernel')->getBundle('LoopitaMetalizadoraBundle')->getPath();
-        return $this->render('MaithCommonTranslatorBundle:Default:index.html.twig', array('name' => 'hello'));
+        $bundle = $this->getRequest()->get("bundle");
+        $lang = $this->getRequest()->get("lang");
+        $path = $this->get('kernel')->getBundle($bundle)->getPath()."/Resources/translations";
+        $loader = $this->get('translation.loader');
+        $catalog = new MessageCatalogue($lang);
+        $loader->loadMessages($path, $catalog);
+        var_dump($catalog->all("messages"));
+        die('aca');
     }
 }
